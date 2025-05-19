dc88e24e37f6d1e6e60daf2e8a5e687763a9dabc
Handi Pradana <113164329+handipradana@users.noreply.github.com>
2025-05-19 13:50:43 +0700

    Update lambda_post.py

--- lambda/lambda_post.py
+++ lambda/lambda_post.py
@@ -2,10 +2,7 @@ import json
 import secrets
 import time
 import boto3
-<<<<<<< HEAD
 import os
-=======
->>>>>>> a5129244e1fb687b958df930a3d59d337f356f93
 from botocore.exceptions import ClientError
 
 def lambda_handler(event, context):
@@ -15,11 +12,7 @@ def lambda_handler(event, context):
         expiration = int(time.time()) + 300  # 5 menit
         
         dynamodb.put_item(
-<<<<<<< HEAD
             TableName=os.environ['TOKEN_TABLE'],
-=======
-            TableName='Tokens',
->>>>>>> a5129244e1fb687b958df930a3d59d337f356f93
             Item={
                 'token': {'S': token},
                 'expiration': {'N': str(expiration)}
